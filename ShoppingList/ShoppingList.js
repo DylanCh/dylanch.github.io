b7d16c3dd9e493a31ed0a870ea5722346ad6da5b
Dylan Chen <hchen004@citymail.cuny.edu>
2016-12-28 18:54:53 -0500

    Update ShoppingList.js

--- ShoppingList/ShoppingList.js
+++ ShoppingList/ShoppingList.js
@@ -4,36 +4,57 @@
     
     shoppingListController.$inject = ['$scope'];
     
+    shoppingListApp.filter('hideBoughtItems',function(){
+        
+    });
+    
     function shoppingListController($scope){
         $scope.toBuy=[
              cookies={
-                name:'cookies',quantity:10
+                name:'cookies',quantity:0,bought:false
             },
             water={
-                name:'water',quantity:3
+                name:'water',quantity:0,bought:false
             },
             bananas={
-                name:'bananas',quantity:14
+                name:'bananas',quantity:0,bought:false
             },
             milk={
-                name:'milk',quantity:1
+                name:'milk',quantity:0,bought:false
             },
             coconut={
-                name:'coconut',quantity:1
+                name:'coconut',quantity:0,bought:false
             }
         ];  // end array     
         
         $scope.boughtList=[];
         $scope.btnOnClick = function addAndDropItems(itemName){
-        if(itemName === 'cookies'){
-            $scope.toBuy[0].quantity +=10;
-            //console.log($scope.toBuy[0].quantity);
-            $scope.boughtList.push($scope.toBuy[0]);
-        }
-    };
+            var index;
+            if(itemName === 'cookies'){
+                index=0;
+            }
+            else if(itemName === 'water'){
+                index=1;
+            }
+            else if(itemName ==='bananas'){
+                index=2;
+            }
+            else if(itemName===''){
+                index=3;
+            }
+            else if (itemName===''){
+                index=4;
+            }
+            $scope.toBuy[index].quantity +=10;
+            $scope.toBuy[index].bought = true;
+            console.log($scope.toBuy[index].quantity);
+            $scope.boughtList.push($scope.toBuy[index]);
+    }; // end function
         
     }; // end controller
     
     
     
 }());
+
+
