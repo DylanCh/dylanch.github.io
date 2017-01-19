7c46d5ba5e45cb2f1d897f706b30c65c45c9594e
Dylan Chen <hchen004@citymail.cuny.edu>
2017-01-19 13:43:57 -0500

    Add files via upload

--- DavidsRestaurant/data/data.service.js
+++ DavidsRestaurant/data/data.service.js
@@ -1,26 +1,30 @@
-(function(){
-    
-    function dataService(){
-        var myself = this;
-        myself.customerInformation = {
-            firstName: '',
-            lastName: '',
-            emailAddress: '',
-            phoneNumber: '',
-            menuNumber: '',
-            registered: false
-        };
+(function () {
+  "use strict";
 
-        myself.getCustomerInformation = function() {
-          return myself.customerInformation;
-        };
+  angular.module('data').service('DataService', DataService);
 
-        myself.setCustomerInformation = function(customer) {
-          myself.customerInformation = customer;
-        };
-    };// end function
-    
-    var app=angular.module('data');
-    app.service('DataService',dataService);
-});
+  //DataService.$inject = [];
 
+  // singleton ???
+  function DataService() {
+    var myself = this;
+    myself.customerInformation = {
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+      phoneNumber: '',
+      menuNumber: '',
+      registered: false
+    };
+
+    myself.getCustomerInformation = function() {
+      return myself.customerInformation;
+    };
+
+    myself.setCustomerInformation = function(customer) {
+      myself.customerInformation = customer;
+    };
+
+  }
+
+})();
