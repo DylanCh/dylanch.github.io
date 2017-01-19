e1d0c7566f88d0caabd95fc8bec75f6475303f8a
Dylan Chen <hchen004@citymail.cuny.edu>
2017-01-19 13:49:47 -0500

    Add files via upload

--- DavidsRestaurant/src/public/signup-form/signupform.controller.js
+++ DavidsRestaurant/src/public/signup-form/signupform.controller.js
@@ -1,24 +1,32 @@
 (function() {
-    var app = angular.module('public');  
-    
-    SignupController.$inject = ['DataService', 'menuItems'];
-    function SignupController(DataService, menuItems) {
-        var myself = this;
-        myself.customer = DataService.getCustomerInformation();
-        myself.menuNumberExists = true;
 
-        myself.submitForm = function() {
-          myself.customer.registered = true;
-          DataService.setCustomerInformation(myself.customer);
-          if(menuItems.indexOf(myself.customer.menuNumber) <0) {
-            myself.menuNumberExists = false;
-          } else {
-            myself.menuNumberExists = true;
-            alert('Congratulations you have signed up.');
-          }
+  var signupPage =angular.module('public');
+  signupPage.controller("SignupController", SignupController);
+  
+  SignupController.$inject = ['DataService', 'menuItems'];
+  function SignupController(DataService, menuItems) {
+    var myself = this;
+    myself.customer = DataService.getCustomerInformation();
+    myself.menuNumberExists = true;
+    
+    myself.resetForm=function(){
+            jQuery('#signUpForm')[0].reset();
+//            console.log(jQuery('#signUpForm')[0]);
     };
-    
-    
-  }; // end function
-  app.controller("SignupController", SignupController);
+
+    myself.goback=function(){
+        if(confirm('Cancel sign up?'))
+            window.history.back();
+    };
+
+    myself.submitForm = function() {
+      myself.customer.registered = true;
+      DataService.setCustomerInformation(myself.customer);
+      if(menuItems.indexOf(myself.customer.menuNumber) <0) {
+        myself.menuNumberExists = false;
+      } else {
+        myself.menuNumberExists = true;
+      }
+    };
+  }
 })();
