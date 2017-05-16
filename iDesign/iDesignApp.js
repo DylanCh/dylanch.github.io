267117123e5b3d815f33dab386a64136bae9aedb
Dylan Chen <hchen004@citymail.cuny.edu>
2017-05-16 02:06:51 -0400

    Add files via upload

---
+++ iDesign/iDesignApp.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#comp-j2qt7cwtlink').click(function(){alert('OK');});
+    $('#comp-j2qt806ulink').click(function(){alert('OK');});
+    $('#comp-j2qtja5jlink').click(function(){alert('OK');});
+    $('#comp-j2qtja5olink').click(function(){alert('OK');});
+    $('#comp-j2qrbf64link').click(function(){alert('Thank you for your input!');});
+    $('#comp-j2qrbt4qlink').click(function(){alert('Thank you for your input!');});
+    $('#comp-j2qr2un2link').click(function(){ alert('Thank you for your input!'); });
+    $('#comp-j2qr3iwjlink').click(function(){ alert('Thank you for your input!'); });
+    $('#comp-j2qu8cp0link').click(function(){alert('Thank you for your input!');});
+    $('#comp-j2qu8ln3link').click(function(){alert('Thank you for your input!');});
+});
