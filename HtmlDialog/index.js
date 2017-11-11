78c3fb1a7ce3a5e9f814cdd1a5c8ea443c49859b
Hanjun Chen <hchen004@citymail.cuny.edu>
2017-11-10 22:07:48 -0500

    Create index.js

---
+++ HtmlDialog/index.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @param {Boolean} modal 
+ * @param {Boolean} resizable 
+ * @param {String} width 
+ * @param {String} title 
+ */
+var dialogBox = function (modal,resizable,width,title){
+    this.modal = modal;
+    this.resizable = resizable;
+    this.width = width;
+    this.title = title;
+}
+
+(function($){
+    function openTerms(event){
+        try{
+            $('#terms').dialog(new dialogBox(true,false,'500px','DISCLAIMER'));
+        }
+        catch(error){
+            console.dir(error);
+        }
+    }
+
+    $(document).ready(function(){
+        $('#open_terms').click(openTerms);
+
+        $('#iUnderstandBtn').click(function(e){
+            $(e.target).closest('.ui-dialog-content').dialog('close'); 
+        });
+    });
+})(jQuery);
