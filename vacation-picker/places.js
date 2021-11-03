84d6937278f4dd60b605ddf70a9142a8b3a8950f
H Chen <hchen004@citymail.cuny.edu>
2021-11-03 00:52:10 -0400

    Create places.js

---
+++ vacation-picker/places.js
@@ -0,0 +1,70 @@
+const places = [
+  {
+    state: 'FL',
+    places: [
+      {
+        name: 'Clearwater Beach',
+        map:'https://maps.apple.com/?address=Gulf%20of%20Mexico%20Dr,%20Longboat%20Key,%20FL%2034228,%20United%20States&ll=27.982654,-82.829254'
+      },
+      {
+        name: 'Miami Beach',
+        map:'https://www.google.com/maps/place/Miami+Beach,+FL/@25.8102247,-80.2101823,12z'
+      },
+      {
+        name: 'Daytona Beach',
+        map: 'https://maps.apple.com/?address=Daytona%20Beach,%20FL,%20United%20States&ll=29.206762,-81.020553'
+      },
+      {
+        name: 'Pensacola Beach',
+        map: 'https://www.google.com/maps/place/Pensacola+Beach,+FL/@30.3541359,-87.1915156,11z'
+      },
+      {
+        name:'Universal Studios',
+        map: 'https://maps.apple.com/?address=6000%20Universal%20Boulevard,%20Orlando,%20FL%2032819,%20United%20States&ll=28.476312,-81.468430'
+      }
+    ]
+  },
+  {
+    state: 'NY',
+    places:[
+      {
+        name: 'Little Island @Pier55',
+        map: 'https://maps.apple.com/?address=West%2013th%20Street,%20New%20York,%20NY%2010014,%20United%20States&ll=40.741990,-74.010633'
+      },
+      {
+        name: 'Empire State Building',
+        map: 'https://maps.apple.com/?address=350%205th%20Ave,%20New%20York,%20NY%20%2010118,%20United%20States&ll=40.748441,-73.985664'
+      },
+      {
+        name: 'Kaaterskill Falls',
+        map: 'https://www.google.com/maps/place/Kaaterskill+Falls/@42.1934368,-74.0719472,15z'
+      },
+      {
+        name: 'Yankee Stadium',
+        map: 'https://maps.apple.com/?address=1%20East%20161st%20Street,%20Bronx,%20NY%2010451,%20United%20States&ll=40.829617,-73.926342'
+      },
+      {
+        name: 'Coney Island',
+        map: 'https://maps.apple.com/?address=Coney%20Island,%20Brooklyn,%20New%20York,%20NY,%20United%20States&ll=40.578972,-73.985307'
+      },
+      {
+        name: 'Robert Moses State Park',
+        map: 'https://maps.apple.com/?address=600%20Robert%20Moses%20State%20Pkwy,%20Babylon,%20NY%2011702,%20United%20States&ll=40.624870,-73.266320'
+      },
+      {
+        name: 'World Trade Center',
+        map:'https://maps.apple.com/?address=World%20Trade%20Center,%20Manhattan,%20New%20York,%20NY,%20United%20States&ll=40.711770,-74.012360'
+      }
+    ]
+  },
+  {
+    state: 'VA',
+    places:[
+      {
+        name:'Battelship USS Wisconsin',
+        map:'https://maps.apple.com/?address=101%20Boush%20St,%20Norfolk,%20VA%20%2023510,%20United%20States&ll=36.848430,-76.293158'
+      }
+    ]
+  }
+
+];
