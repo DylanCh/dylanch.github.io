02ec76fca415bab1efa89031049d9d10a51abb6f
H Chen <hchen004@citymail.cuny.edu>
2021-11-03 22:04:21 -0400

    display regions

--- vacation-picker/app.js
+++ vacation-picker/app.js
@@ -1,3 +1,5 @@
+const regionSort = (x,y)=> { if(x.region > y.region) return 1; if(x.region<y.region) return -1; return 0 };
+
 window.onload = () => {
   for(const state of places){
     var card = document.createElement('div');
@@ -6,7 +8,7 @@ window.onload = () => {
       `<div class="card-header">${state.state}</div>
           <div class="card-body"><table class="table">
               <thead><tr><th>Place</th><th>Map</th><th>Have I been?</th></tr></thead>
-              <tbody>${state.places.map(x => '<tr><td>'+x.name + '</td><td><a href="'+ x.map +'" target="_blank">Map</a></td><td><input type="checkbox"/></td></tr>').join('\n')}</tbody>
+              <tbody>${state.places.sort(regionSort).map(x => '<tr><td>'+x.name + '</td><td><a href="'+ x.map +'">'+ x.region +'</a></td><td><input type="checkbox"/></td></tr>').join('\n')}</tbody>
             </table>
           </div>
         </div>`);
