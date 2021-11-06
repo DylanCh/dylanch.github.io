c6cf48c3d7f48f8433d08165b75a23945fa9fe75
H Chen <hchen004@citymail.cuny.edu>
2021-11-06 15:39:14 -0400

    Tabs (#6)
    * Update app.js
    
    * Create add.js
    
    * Update add.html
    
    * Update add.html
    
    * Update places.html
    
    * Update add.html

--- vacation-picker/app.js
+++ vacation-picker/app.js
@@ -31,44 +31,5 @@ window.onload = () => {
         </div>`);
 
     document.getElementById('root').append(card);
-    
-    for(const state of availableStates.sort()){
-      let option = document.createElement('option');
-      option.value = state;
-      option.innerText = state;
-      document.getElementById('state').append(option);
-    }
-    
-    const form = document.getElementsByTagName('form')[0];
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      const state = document.getElementById('state').value;      
-      if(!state || availableStates.indexOf(state) < 0){ alert(`${state} is not a valid state`); return }
-      
-      let added = { 
-        state, 
-        places: [ 
-          { 
-            name: document.getElementById('attraction').value,
-            map: document.getElementById('map').value,
-            region: document.getElementById('region').value, 
-            checked: true  
-          } 
-        ]
-       };
-      
-      let selectedState = places.filter(x => x.state === state);
-      if(!selectedState.length <= 0){
-        places.push(added);
-      }
-      else{
-        if(selectedState[0].places.length === 0)
-          selectedState[0].places = added.places;
-        else selectedState[0].places.push(added.places[0]);
-      }
-      
-      localStorage['places'] = JSON.stringify(places);
-      window.location.reload(true);
-    });
   }
 };
