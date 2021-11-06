31cc6543fc3dd52c2dd0c16726b9f38de0f60387
H Chen <hchen004@citymail.cuny.edu>
2021-11-06 01:11:38 -0400

    Update app.js

--- vacation-picker/app.js
+++ vacation-picker/app.js
@@ -1,7 +1,7 @@
-const regionSort = (x,y)=> { if(x.region > y.region) return 1; if(x.region<y.region) return -1; return 0 };
-
 window.onload = () => {
+  const regionSort = (x,y)=> { if(x.region > y.region) return 1; if(x.region<y.region) return -1; return 0 };
   const availableStates = ["WA","OR","CA","NV","ID","MT","WY","UT","AZ","NM","CO","AK","HI","TX","OK","KS","NE","SD","ND","MN","IA","WI","MO","AR","LA","MS","IL","IN","KY","TN","AL","GA","MI","NY","VT","NH","ME","OH","PA","MA","CT","RI","NJ","DE","DC","MD","WV","VA","NC","SC","FL"];
+  
   if(!localStorage['places']){
     localStorage.setItem('places',JSON.stringify(places));
   }
@@ -9,7 +9,7 @@ window.onload = () => {
     var card = document.createElement('div');
     card.className = 'card';
     
-    var tBody;
+    let tBody;
     if(state.places){
       tBody= state.places
         .sort(regionSort)
@@ -47,7 +47,7 @@ window.onload = () => {
       
       let added = { 
         state, 
-        place: [ 
+        places: [ 
           { 
             name: document.getElementById('attraction').value,
             map: document.getElementById('map').value,
