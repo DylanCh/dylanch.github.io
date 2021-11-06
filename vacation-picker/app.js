77419ae7f8f1859d7ea8cb496a1a13394f23b92f
H Chen <hchen004@citymail.cuny.edu>
2021-11-06 01:06:39 -0400

    Update app.js

--- vacation-picker/app.js
+++ vacation-picker/app.js
@@ -8,15 +8,23 @@ window.onload = () => {
   for(const state of JSON.parse(localStorage['places'])){
     var card = document.createElement('div');
     card.className = 'card';
+    
+    var tBody;
+    if(state.places){
+      tBody= state.places
+        .sort(regionSort)
+        .map(x => '<tr><td>'+ x.name +'&nbsp;&nbsp;<a href="https://duckduckgo.com/?q='+ encodeURI(x.name) +'&t=h_&iax=images&ia=images">🔎</a></td><td>'
+            + x.region +'</td><td><a href="'+ x.map +'">Map</a></td><td><input type="checkbox" /></td></tr>').join('\n');
+    }
+    else{
+      tBody = '<tr><td span="4">I have not visited attractions in ' + state.state + '</td></tr>';
+    }
     card.innerHTML = (
       `<div class="card-header">${state.state}</div>
           <div class="card-body"><table class="table">
               <thead><tr><th>Place</th><th>Region</th><th>Map</th><th>Have I been?</th></tr></thead>
               <tbody>
-                ${state.places
-                .sort(regionSort)
-                .map(x => '<tr><td>'+ x.name +'&nbsp;&nbsp;<a href="https://duckduckgo.com/?q='+ encodeURI(x.name) +'&t=h_&iax=images&ia=images">🔎</a></td><td>'
-                    + x.region +'</td><td><a href="'+ x.map +'">Map</a></td><td><input type="checkbox" /></td></tr>').join('\n')}
+                ${tBody}
               </tbody>
             </table>
           </div>
