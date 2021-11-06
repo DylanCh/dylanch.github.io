0577129528c37d8391e8c7a749033c9da4984c17
H Chen <hchen004@citymail.cuny.edu>
2021-11-05 23:10:56 -0400

    Update app.js

--- vacation-picker/app.js
+++ vacation-picker/app.js
@@ -30,7 +30,7 @@ window.onload = () => {
     }
     
     const form = document.getElementsByTagName('form')[0];
-    form.addEventListener(e => {
+    form.addEventListener('submit', e => {
       e.preventDefault();
       const formData = new FormData(form);
       const state = formData['state'];
